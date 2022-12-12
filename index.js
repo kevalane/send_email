a6c950dd2b77d047581986f000fea0e0028c15ab
Kevin Rasmusson <kevin.rasmusson@hotmail.com>
2022-12-11 23:23:13 -0800

    initial commit

---
+++ index.js
@@ -0,0 +1,56 @@
+const aws = require('aws-sdk');
+const sesConfig = {
+    apiVersion: '2010-12-01',
+    region: 'eu-central-1',
+}
+const ses = new AWS.SES(sesConfig)
+
+export const handler =  async (event, context) => {
+    // const incomingMsg = JSON.parse(event.Records[0].body);
+    const msg = "Hello World from Lambda & SES!";
+    const html = "<p>Hello World from Lambda & SES using HTML</p>";
+    const params = {
+        Destination: {
+            ToAddresses: ["kevin@jkmholding.com"]
+        },
+        Message: {
+            Body: {
+                Html: {
+                    Charset: 'UTF-8',
+                    Data: html
+                },
+                Text: {
+                    Charset: 'UTF-8',
+                    Data: msg
+                }
+            },
+            Subject: {
+                Charset: 'UTF-8',
+                Data: "HELLO WORLD"
+            }
+        },
+        Source: "info@jkmholding.com",
+        ReplyToAddresses: [
+            "kevras.contact@gmail.com"
+        ]
+    }
+    const sendEmail = new AWS.SES({apiVersion: '2010-12-01'}).sendEmail(params).promise();
+    sendEmail.then(data => {
+    // await ses.sendEmail(params).promise().then((data) => {
+        console.log(data)
+        const response = {
+            "statusCode": 200,
+            "body": JSON.stringify(data),
+            "isBase64Encoded": false
+        };
+        return response;
+    }).catch((err) => {
+        console.error(err)
+      	const response = {
+            "statusCode": 501,
+            "body": JSON.stringify(err),
+            "isBase64Encoded": false
+        };
+        return response;
+    });
+}
